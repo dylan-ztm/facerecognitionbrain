7a811b5bb34255dbd6c46a4cef410ab8cd418a04
dylan-ztm <dccnation@gmail.com>
2020-07-02 19:23:51 -0700

    Initial commit of facerecognitionbrain frontend, master branch.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,172 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Navigation from './components/Navigation/Navigation';
+import Logo from './components/Logo/Logo';
+import Rank from './components/Rank/Rank';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
+import Signin from './components/Signin/Signin';
+import Register from './components/Register/Register';
+import Particles from 'react-particles-js';
+import Clarifai from 'clarifai';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+//global constant to store API key in app object.
+const app = new Clarifai.App({
+  apiKey: '65eed300601c447a963934e13b2f0dc5',
+ });
+
+//global constant to configure Particles Options.
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 55,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    }
+  }
+} //end particlesOptions constant
+
+const initialState = { 
+                      input: '',
+                      imageUrl: '',
+                      box: {},
+                      route: 'signin',
+                      isSignedIn: false,
+                      user: {
+                            id: '',
+                            name: '',
+                            email: '',
+                            entries: 0,
+                            joined: '',
+                      }
+} //end of initialState constant
+
+//App Component
+class App extends Component { //changed function App to class App
+  constructor() {
+    super();
+    this.state = initialState;
+  } //end constructor
+
+  loadUser = (data) => {
+    this.setState({
+      user: {
+              id: data.id,
+              name: data.name,
+              email: data.email,
+              entries: data.entries,
+              joined: data.joined
+        }
+    }) //end setState
+  } //end loadUser
+
+  //calculateFaceLocation
+  calculateFaceLocation = (data) => {
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputimage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow:  clarifaiFace.top_row * height,
+      rightCol: width - (clarifaiFace.right_col * width),
+      bottomRow: height - (clarifaiFace.bottom_row * height)
+    }
+  } //end calculateFaceLocation
+
+  //displayFaceBox
+  displayFaceBox = (box) => {
+    this.setState({box: box});
+  } //displayFaceBox
+
+  //onInputChange. Used for the input field in ImageLinkForm.
+  onInputChange = (event) => {
+    this.setState({input: event.target.value});
+  } //end onInputChange
+
+  //onButtonSubmit. Used for the "Detect" button being clicked.
+  onButtonSubmit = () => {
+    this.setState({imageUrl: this.state.input});
+    app.models
+      .predict(
+        Clarifai.FACE_DETECT_MODEL, 
+        this.state.input)
+      .then(response => {
+        if (response) {
+          fetch('http://localhost:3001/image', {
+            method: 'put',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+              id: this.state.user.id
+            })
+          })
+          .then(response => response.json())
+          .then(count => {
+            this.setState(Object.assign(this.state.user, { entries: count}));
+          })
+          .catch(console.log)
+        }
+        this.displayFaceBox(this.calculateFaceLocation(response))
+      })
+      .catch(error => console.log(error));
+  }// end onButtonSubmit
+
+  //onRouteChange
+  onRouteChange = (route) => {
+    if (route === 'signout') {
+      this.setState(initialState)
+    } else if (route === 'home') {
+      this.setState({isSignedIn: true})
+    }
+    this.setState({route: route});
+  } //end onRouteChange
+
+  //render
+  render() {
+    const { isSignedIn, imageUrl, route, box } = this.state;
+    return (
+      <div className="App">
+        <Particles className='particles' 
+                   params={particlesOptions} 
+        />
+        <Navigation isSignedIn={isSignedIn} 
+                    onRouteChange={this.onRouteChange}/>
+        { route === 'home'
+          ? <div> 
+              <Logo />
+              <Rank 
+                  name={this.state.user.name}
+                  entries={this.state.user.entries}
+              />
+              <ImageLinkForm 
+                  onInputChange={this.onInputChange}
+                  onButtonSubmit={this.onButtonSubmit}
+              />
+              <FaceRecognition box={box} imageUrl={imageUrl}/>
+            </div>
+          : (
+              route === 'signin'
+              ? <Signin   
+                      onRouteChange={this.onRouteChange}
+                      loadUser={this.loadUser}
+                />
+              : (
+                  route === 'register'
+                  ? <Register 
+                        onRouteChange={this.onRouteChange}
+                        loadUser={this.loadUser}
+                    />
+                  : <Signin   onRouteChange={this.onRouteChange}
+                              loadUser={this.loadUser}
+                  />
+              )
+            )
+        }
+      </div>
+    );
+  }
+} //end render
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import 'tachyons'; //tachyons can go here.  
+                   //will be used by all child components.
 
 ReactDOM.render(
   <React.StrictMode>
